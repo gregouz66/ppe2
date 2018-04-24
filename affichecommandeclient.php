a312e42ec71846a4a6b42608212382c24015a2b3
gregouz66 <gregory.cascales@gmail.com>
2018-04-24 19:42:42 +0200

    HTML finit sur commandeclient, un client inscrit peut mnt ajouter une adresse par defaut

--- affichecommandeclient.php
+++ affichecommandeclient.php
@@ -1,54 +1,18 @@
 <?php include ('inc/bdd.php'); ?>
 
 <?php
+//Formulaire infos livraisons
+include ('inc/commandes/ajout_adressedefaut.php');
+
 // REQUETE POUR SAVOIR SI LE CLIENT A UNE ADRESSE PAR DEFAUT
 $id_client = $_SESSION['id_client'];
 $adrpardef = $bdd->prepare("SELECT * FROM adresseclient WHERE id_client = ? AND adressedefaut_adresseclient = 1 LIMIT 1");
 $adrpardef->execute(array($id_client));
 $adrexist = $adrpardef->rowCount();
-
-//Formulaire infos livraisons
-if (isset($_POST['valider'])){
-  $id_client = $_SESSION['id_client'];
-  $Adresse=htmlentities(trim($_POST['Adresse']));
-  $Prenom_livr=htmlentities(trim($_POST['Prenom_livr']));
-  $Nom_livr=htmlentities(trim($_POST['Nom_livr']));
-  $Societe=htmlentities(trim($_POST['Societe'])); //PAS OBLIGATOIRE
-  $CP=htmlentities(trim($_POST['Cp']));
-  $etat_prov=htmlentities(trim($_POST['etat_province']));
-  $Ville=htmlentities(trim($_POST['Ville']));
-  $Pays=htmlentities(trim($_POST['Pays']));
-  $adr_defaut = 1;
-
-  $nomcomplet_client = $Prenom_livr . ' ' . $Nom_livr; //Réuni prenom + nom
-
-  if(!empty($_POST['Adresse']) AND !empty($_POST['Prenom_livr']) AND !empty($_POST['Nom_livr']) AND !empty($_POST['Cp']) AND !empty($_POST['etat_province']) AND !empty($_POST['Ville']) AND !empty($_POST['Pays'])) {
-
-    //Verifie code postal contient 5 chiffres ET seulement des chiffres
-    $verif_CP = strlen($CP);
-    if(is_numeric($CP) AND $verif_CP==5){
-
-      // // Création adresse livraison du client anonyme
-      $AdresseLivraison = $bdd->prepare("INSERT INTO adresseclient(id_client,libelle_adresseclient,nomcomplet_adresseclient,societe_adresseclient,codepostal_adresseclient,etatprovince_adresseclient,ville_adresseclient,pays_adresseclient,adressedefaut_adresseclient) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
-      $AdresseLivraison->execute(array($id_client, $Adresse, $nomcomplet_client, $Societe, $CP, $etat_prov, $Ville, $Pays, $adr_defaut));
-      $result_livr = $AdresseLivraison->rowCount();
-
-      if($result_livr == 1){
-        $errors1[] = "L'adresse de livraison a été ajouté par defaut avec succès !";
-      } else {
-        $errors[] = "Erreur lors de l'ajout de l'adresse de livraison !";
-        var_dump($AdresseLivraison->errorInfo());
-      }
-    } else {
-      $errors[] = "Le code postal doit contenir 5 chiffres et/ou doit contenir seulement des chiffres !";
-    }
-  } else {
-    $errors[] = "Les champs obligatoires doivent être remplis !";
-  }
-}
 ?>
 
 <?php include './inc/header.php' ?>
+<link rel="stylesheet" href="assets/css/commandes.css" />
 
   <div id="wrapper">
   <div id="main">
@@ -80,92 +44,126 @@ if (isset($_POST['valider'])){
 
     <h3>Compte</h3>
 
-      <label>Mail*</label>
       <input type="text" name="Mail" placeholder="mail" value="<?php echo $_SESSION['email_client'] ?>" disabled/>
 
-      <label>Prenom*</label>
       <input type="text" name="Prenom" placeholder="Prenom" value="<?php echo $_SESSION['prenom_client'] ?>" disabled/>
 
-      <label>Nom*</label>
       <input type="text" name="Nom" placeholder="Nom" value="<?php echo $_SESSION['nom_client'] ?>" disabled/>
 
-      <br>
+      <hr>
 
-      <h3>Adresse de livraison</h3>
+      <?php if($adrexist == 1) { ?>
+      <div class="row">
+        <div class="col-md-6 col-sm-6" style="border-right: solid;">
+        <?php } ?>
 
-      <!-- SI LE CLIENT N'A AUCUNE ADRESSE PAR DEFAUT EXISTANTE ALORS IL PEUT EN CREER UNE -->
-      <?php if($adrexist == 0) { ?>
+          <h3>Adresse de livraison</h3>
+
+          <!-- SI LE CLIENT N'A AUCUNE ADRESSE PAR DEFAUT EXISTANTE ALORS IL PEUT EN CREER UNE -->
+          <?php if($adrexist == 0) { ?>
+
+            <form id="form" method="POST" action="#">
+
+            <label>Prenom*</label>
+            <input type="text" name="Prenom_livr" placeholder="Prenom"/>
+
+            <label>Nom*</label>
+            <input type="text" name="Nom_livr" placeholder="Nom"/>
+
+            <label>Societe(optionel)</label>
+            <input type="text" name="Societe" placeholder="Société"/>
+
+            <label>Adresse*</label>
+            <input type="text" name="Adresse" placeholder="Adresse"/>
+
+            <label>Pays*</label>
+            <select name="Pays">
+            <option value ="France">France</option>
+            </select>
+
+            <label>Etat / Province*</label>
+            <select name="etat_province">
+            <option value ="Occitanie">Occitanie</option>
+            </select>
+
+            <label>Ville*</label>
+            <input type="text" name="Ville" placeholder="Ville"/>
+
+            <label>Code Postal*</label>
+            <input type="text" name="Cp" placeholder="Code Postal"/>
+
+            </br>
+
+            <input style="font-size: 22px;" type="submit" name="valider"/>
+
+          </form>
+
+        <?php } else {
+          $fetchall_adrpardef = $adrpardef->fetchAll();
+          foreach($fetchall_adrpardef as $row) { ?> <!--SI LE CLIENT POSSEDE DEJA UNE ADRESSE PAR DEFAUT ALORS LES INFOS S'AFFICHES-->
+
+            <div class="col-md-6 col-sm-6" style="float: left;">
+
+              <label><?php echo $row['nomcomplet_adresseclient'] ?></label>
+
+              <?php if($row['societe_adresseclient'] != ""){?>
+              <label><?php echo $row['societe_adresseclient'] ?></label>
+              <?php } ?>
+
+              <label><?php echo $row['libelle_adresseclient'] ?></label>
+
+              <label><?php echo $row['pays_adresseclient'] ?></label>
+
+              <label><?php echo $row['etatprovince_adresseclient'] ?></label>
+
+              <label><?php echo $row['ville_adresseclient'] ?></label>
+
+              <label><?php echo $row['codepostal_adresseclient'] ?></label>
+            </div>
+
+            <div class="col-md-6 col-sm-6" style="float: left;">
+              <a href='#'><button>Modifier</button></a>
+            </div>
+
+
+        <?php } ?>
+      </div>
+      <div class="col-md-6 col-sm-6">
+
+        <h3>Option de livraison</h3>
 
         <form id="form" method="POST" action="#">
+          <input type="radio" class="" name="option_livr" value="0"/> <label>Livraison Standard (GRATUIT)</label>
+          <br>
+          <input type="radio" class="" name="option_livr" value="1"/> <label>Livraison 24h (10€)</label>
+        </form>
 
-        <label>Prenom*</label>
-        <input type="text" name="Prenom_livr" placeholder="Prenom"/>
+      </div>
+    </div>
 
-        <label>Nom*</label>
-        <input type="text" name="Nom_livr" placeholder="Nom"/>
+    <hr>
 
-        <label>Societe(optionel)</label>
-        <input type="text" name="Societe" placeholder="Société"/>
-
-        <label>Adresse*</label>
-        <input type="text" name="Adresse" placeholder="Adresse"/>
-
-        <label>Pays*</label>
-        <select name="Pays">
-        <option value ="France">France</option>
-        </select>
-
-        <label>Etat / Province*</label>
-        <select name="etat_province">
-        <option value ="Occitanie">Occitanie</option>
-        </select>
-
-        <label>Ville*</label>
-        <input type="text" name="Ville" placeholder="Ville"/>
-
-        <label>Code Postal*</label>
-        <input type="text" name="Cp" placeholder="Code Postal"/>
-
-        </br>
-
-        <input style="font-size: 22px;" type="submit" name="valider"/>
-
-      </form>
-
-    <?php } else {
-      $fetchall_adrpardef = $adrpardef->fetchAll();
-      foreach($fetchall_adrpardef as $row) { ?> <!--SI LE CLIENT POSSEDE DEJA UNE ADRESSE PAR DEFAUT ALORS LES INFOS S'AFFICHES-->
-
-      <label>Nom complet</label>
-      <input type="text" name="Prenom_livr" value="<?php echo $row['nomcomplet_adresseclient'] ?>" disabled/>
-
-      <label>Societe</label>
-      <input type="text" name="Societe" value="<?php echo $row['societe_adresseclient'] ?>" disabled/>
-
-      <label>Adresse</label>
-      <input type="text" name="Adresse" value="<?php echo $row['libelle_adresseclient'] ?>" disabled/>
-
-      <label>Pays</label>
-      <select name="Pays" disabled>
-      <option><?php echo $row['pays_adresseclient'] ?></option>
-      </select>
-
-      <label>Etat / Province</label>
-      <select name="etat_province" disabled>
-      <option><?php echo $row['etatprovince_adresseclient'] ?></option>
-      </select>
-
-      <label>Ville</label>
-      <input type="text" name="Ville" value="<?php echo $row['ville_adresseclient'] ?>" disabled/>
-
-      <label>Code Postal</label>
-      <input type="text" name="Cp" value="<?php echo $row['codepostal_adresseclient'] ?>" disabled/>
-
-      <br>
-
-      <a href='#'>Changer d'adresse de livraison par défaut</a>
-
-    <?php } } ?>
+    <div class="row">
+      <div class="col-md-3 col-sm-6" style="border-right: solid; padding-right: 15px;">
+        <h3>Paiement</h3>
+        <div class="payment-methods">
+          <ul>
+            <li>
+              <button class="btn secondary payment-method card" style="background-image: url(&quot;https://assets.asosservices.com/asos-finance/images/paymentmethods/card.png&quot;);">Ajouter une carte de crédit/débit</button>
+              <label>Ou</label>
+            </li>
+            <li>
+              <button class="btn secondary payment-method payPal" style="background-image: url(&quot;https://assets.asosservices.com/asos-finance/images/paymentmethods/paypal.png&quot;);">PayPal</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-9 col-sm-6">
+        <h3>Nous acceptons :</h3>
+        <img alt="" src="https://assets.asosservices.com/asos-finance/images/marketing/fr/single.png">
+      </div>
+    </div>
+    <?php } ?>
 
 </article>
 </div>
---
+++ inc/commandes/ajout_adressedefaut.php
@@ -0,0 +1,40 @@
+<?php
+//Formulaire infos livraisons
+if (isset($_POST['valider'])){
+  $id_client = $_SESSION['id_client'];
+  $Adresse=htmlentities(trim($_POST['Adresse']));
+  $Prenom_livr=htmlentities(trim($_POST['Prenom_livr']));
+  $Nom_livr=htmlentities(trim($_POST['Nom_livr']));
+  $Societe=htmlentities(trim($_POST['Societe'])); //PAS OBLIGATOIRE
+  $CP=htmlentities(trim($_POST['Cp']));
+  $etat_prov=htmlentities(trim($_POST['etat_province']));
+  $Ville=htmlentities(trim($_POST['Ville']));
+  $Pays=htmlentities(trim($_POST['Pays']));
+  $adr_defaut = 1;
+
+  $nomcomplet_client = $Prenom_livr . ' ' . $Nom_livr; //Réuni prenom + nom
+
+  if(!empty($_POST['Adresse']) AND !empty($_POST['Prenom_livr']) AND !empty($_POST['Nom_livr']) AND !empty($_POST['Cp']) AND !empty($_POST['etat_province']) AND !empty($_POST['Ville']) AND !empty($_POST['Pays'])) {
+
+    //Verifie code postal contient 5 chiffres ET seulement des chiffres
+    $verif_CP = strlen($CP);
+    if(is_numeric($CP) AND $verif_CP==5){
+
+      // // Création adresse livraison du client anonyme
+      $AdresseLivraison = $bdd->prepare("INSERT INTO adresseclient(id_client,libelle_adresseclient,nomcomplet_adresseclient,societe_adresseclient,codepostal_adresseclient,etatprovince_adresseclient,ville_adresseclient,pays_adresseclient,adressedefaut_adresseclient) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+      $AdresseLivraison->execute(array($id_client, $Adresse, $nomcomplet_client, $Societe, $CP, $etat_prov, $Ville, $Pays, $adr_defaut));
+      $result_livr = $AdresseLivraison->rowCount();
+
+      if($result_livr == 1){
+        $errors1[] = "L'adresse de livraison a été ajouté par defaut avec succès !";
+      } else {
+        $errors[] = "Erreur lors de l'ajout de l'adresse de livraison !";
+        var_dump($AdresseLivraison->errorInfo());
+      }
+    } else {
+      $errors[] = "Le code postal doit contenir 5 chiffres et/ou doit contenir seulement des chiffres !";
+    }
+  } else {
+    $errors[] = "Les champs obligatoires doivent être remplis !";
+  }
+} ?>
